532847933e4594e464b7b9c04d5f5f148a047045
KaterinaLarionova <139127810+KaterinaLarionova@users.noreply.github.com>
2024-09-18 22:00:51 +0300

    адаптивные сетки (#8)
    * адаптивные сетки
    
    * дополнение
    
    * сделана главная страница и каталог частично
    
    * добавление в адаптивных сетках
    
    * дополняет адаптивные декоративные элементы
    
    * добавление меню - гамбургер
    
    * исправление в js
    
    * исправление в js
    
    * анимация векторных изображений
    
    * исправление в js и по линтерам
    
    * исправление стилей
    
    * правки в img
    
    * правка в функции js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,20 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.site-list');
+const navToggle = document.querySelector('.main-nav__toggle--closed');
+
+navMain.classList.remove('site-list--nojs');
+navToggle.classList.remove('main-nav__toggle--nojs');
+
+function burger() {
+  if (navToggle.classList.contains('main-nav__toggle--open')) {
+    navMain.classList.add('site-list--closed');
+    navToggle.classList.remove('main-nav__toggle--open');
+    navToggle.classList.add('main-nav__toggle--closed');
+  } else {
+    navMain.classList.add('site-list--open');
+    navMain.classList.remove('site-list--closed');
+    navToggle.classList.add('main-nav__toggle--open');
+    navToggle.classList.remove('main-nav__toggle--closed');
+  }
+}
+navToggle.addEventListener('click', burger);
